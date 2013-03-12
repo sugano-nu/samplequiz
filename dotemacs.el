0352db5f5bdf75798a22826dd4b157d2a57bd28f
G. Jay Kerns <gkerns@ysu.edu>
2013-03-12 02:40:22 -0400

    updated to the new exporter

--- dotemacs.el
+++ dotemacs.el
@@ -2,31 +2,17 @@
 
 (add-to-list 'load-path (expand-file-name "~/path/to/org-mode/lisp"))
 (add-to-list 'auto-mode-alist '("\\.\\(org\\  |org_archive\\|txt\\)$" . org-mode))
-(require 'org-install)
-(require 'org-habit)
-(require 'org-publish)
-(require 'org-latex)
- (add-to-list 'org-export-latex-packages-alist '("" "listings"))
- (add-to-list 'org-export-latex-packages-alist '("" "color"))
 
 (org-babel-do-load-languages
-      'org-babel-load-languages
-      '((emacs-lisp . t)
-        (latex . t)
-        (R . t)))
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (latex . t)
+   (R . t)))
 
 ;; for inline image display
-  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images)   
-  (add-hook 'org-mode-hook 'org-display-inline-images)
+(add-hook 'org-babel-after-execute-hook 'org-display-inline-images)   
+(add-hook 'org-mode-hook 'org-display-inline-images)
 
-;; CUSTOM stuff
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(org-confirm-babel-evaluate nil)
- '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf\\'" . "evince %s"))))
- '(org-startup-truncated nil))
+;; so Org doesn't ask to confirm every time
+(setq org-confirm-babel-evaluate nil)
 
--- samplequiz.R
+++ samplequiz.R
@@ -1,6 +1,4 @@
 
-set.seed(42)
-
 l <- sample(100, size = 3)
 nl <- sample(100, size = 3)
 A <- matrix(c(l, nl), nrow = 2, byrow = TRUE)
@@ -8,6 +6,10 @@ colnms <- c("grape", "orange", "cherry")
 rownms <- c("likes Obama", "doesn't like Obama")
 cindx <- sample(3, size = 2, replace = TRUE)
 rindx <- sample(2, size = 2, replace = TRUE)
+B <- data.frame(addmargins(A))
+names(B) <- c(colnms, "Total")
+rownames(B) <- c(rownms, "Total")
+B
 
 r <- as.double(sample(3:9, size = 1))
 pr <- sample(11:49, size = 1)/100
@@ -15,11 +17,6 @@ disxs <- sort(rnbinom(10, size = r, prob = pr))
 
 library(distrEx)
 Y <- Nbinom(size = r, prob = pr)
-
-png(filename="plotY.png")
-plot(Y, to.draw.arg = "d")
-
-dev.off()
 
 library(distrEx)
 E(Y)
